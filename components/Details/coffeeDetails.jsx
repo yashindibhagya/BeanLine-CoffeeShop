102288b3e7e8f574b81be5383930d465f3f666a8
Yashindi Bhagya <yashindibhagya@gmail.com>
2025-07-31 14:18:33 +0530

    Update CartScreen to include StatusBar import. Remove unused styles from SearchScreen for cleaner code and improved maintainability.

---
+++ components/Details/coffeeDetails.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const coffeeDetails = () => {
+    return (
+        <View>
+            <Text>coffeeDetails</Text>
+        </View>
+    )
+}
+
+export default coffeeDetails
+
+const styles = StyleSheet.create({})
